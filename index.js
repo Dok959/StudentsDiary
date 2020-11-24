8455521f5430531380e4d1f814e3d5b607780ff7
Dok959 <eliseev99@inbox.ru>
2020-11-24 21:25:20 +0400

    debug

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = http.createServer((req, res)=>{
                     res.end('Error')
                 } else{
                     res.writeHead(200, {
-                        'Content-Type': 'text/html'
+                        'Content-Type': contentType
                     })
                     res.end(data)
                 }
