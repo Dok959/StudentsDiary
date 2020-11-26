be68bde3bfb473f143c31b5640f7efde5c96dd34
Alex <eliseev99@inbox.ru>
2020-11-26 15:31:27 +0400

    allocation of responsibilities

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // главный файл приложения
 const path = require('path')
 
+//подключение модулей
 let filePath = path.join(__dirname, "server")
 const server = require(filePath);
 
+filePath = path.join(__dirname, "router")
+const router = require(filePath);
+
+filePath = path.join(__dirname, "requestHandlers")
+const requestHandlers = require(filePath);
+
+// коллекция обработчиков запроса
+const handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+
 // запуск сервера
-server.start();
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // модуль отвечающий за создание и подключение сервера
 const http = require("http");
+const url = require("url");
 
-function start() {
-  function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+function start(route, handle) {
+    function onRequest(request, response) {
+      let pathname = url.parse(request.url).pathname;
+      console.log("Request for " + pathname + " received.");
+  
+      var content = route(handle, pathname, response);
+  
+    //   response.writeHead(200, {"Content-Type": "text/plain"});
+    //   response.write("Hello World");
+    //   response.write(content);//
+    //   response.end();
+    }
+  
+    http.createServer(onRequest).listen(3000);
+    console.log("Server has started.");
   }
-
-  http.createServer(onRequest).listen(3000);
-  console.log("Server has started.");
-}
 
 exports.start = start;
