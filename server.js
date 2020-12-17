5be9e68ef1f8e8738ea6f69ebeef230ecbe14727
Alex <eliseev99@inbox.ru>
2020-12-17 12:04:27 +0400

    обновлены пакеты, мелкие изменения

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const http = require("http");
 const url = require("url");
 
 function start(route, handle) {
-    function onRequest(request, response) {
-      let pathname = url.parse(request.url).pathname;
+    function onRequest(request, response) {  
+      var postData = "";
+      let pathname = url.parse(request.url).pathname;//может попробовать const
       console.log("Request for " + pathname + " received.");
-  
-      var content = route(handle, pathname, response);
-  
-    //   response.writeHead(200, {"Content-Type": "text/plain"});
-    //   response.write("Hello World");
-    //   response.write(content);//
-    //   response.end();
+
+      request.setEncoding("utf8");
+
+      request.addListener("data", function(postDataChunk) {
+        postData += postDataChunk;
+        console.log("Received POST data chunk '"+
+        postDataChunk + "'.");
+      });
+
+      request.addListener("end", function() {
+        route(handle, pathname, response, postData);
+      });
     }
   
     http.createServer(onRequest).listen(3000);
