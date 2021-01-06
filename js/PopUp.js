3992266a1cd9684a9c74bb0ddad69bc6492dc800
Dok959 <procto.aleks@gmail.com>
2021-01-06 14:45:51 +0400

    Валидация данных

---
+++ js/PopUp.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+    $(".b-popup").show();
+    PopUpShowRegistration()
+}
+
+function PopUpShowAuthorization(){
+    PopUpHideRegistration()
+    $(".authorization").show();
+    $('.authorization .login').focus();
+}
+
+function PopUpShowRegistration(){
+    PopUpHideAuthorization()
+    $(".registration").show();
+    $('.registration .login').focus();
+}
+
+function PopUpHideAuthorization(){
+    $(".authorization").hide();
+}
+
+function PopUpHideRegistration(){
+    $(".registration").hide();
+}
+
+//Функция скрытия PopUp
+function PopUpHide(){
+    $(".b-popup").hide();
+}
---
+++ js/validation.js
@@ -0,0 +1,15 @@
+function validation(stingParse, code){
+    let flag = true
+    stingParse.forEach(element => {
+        flag = /(\s+)|((?!\W|@#))|[\/;.,2'"]/.test(element)
+        if (flag === true){ // если есть недопустимый символ
+            break
+        }
+
+        const len = element.length
+        if (len < 5){
+            flag = false // может убрать?
+            break // сообщить об ошибке ввода
+        }
+    })
+}
