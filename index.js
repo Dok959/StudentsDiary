73c911c13ca83c10c7bc6fc0028eebf447a10138
Dok959 <procto.aleks@gmail.com>
2021-01-15 13:27:18 +0400

    начало замещения архитектуры на express

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 // Точка входа приложения
-const path = require('path')
+
+//считывание конфига
+require('dotenv').config();
 
 //подключение модулей
-let filePath = path.join(__dirname, '/config/server')
-const server = require(filePath)
+const express = require('express'); // подключение express
+const fs = require('fs'); // подключение работы с файлами
 
-filePath = path.join(__dirname, '/config/pathParse')
-const parser = require(filePath)
+// создаем объект приложения
+const app = express();
 
-filePath = path.join(__dirname, '/config/responseHandler')
-const responseHandlers = require(filePath)
+// определение порта
+const PORT = process.env.PORT;
 
+// определяем обработчик для для ведения лога вызовов сервера
+app.use(function(request, response, next){
+     
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get('user-agent')}`;
+    fs.appendFile('server.log', data + '\n', function(){});
+    next();
+});
 
-// запуск сервера
-server.start(__dirname, parser.pathParse, responseHandlers.route)
+// определяем обработчик для маршрута '/'
+app.get('/', function(request, response){  // требуется указывать все пути, иначе будет ошибка
+     
+    // отправляем ответ
+    response.send('<h2>Привет Express!</h2>');
+});
+
+// начинаем прослушивать подключения на 3000 порту
+app.listen(PORT, () => {
+    console.log(`Server has been started and listening at http://localhost:${PORT}`);
+});
